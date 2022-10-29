d6db2f9f3c82dc5f43c9c80b6804c67ec43eb9a0
dawndst <116832286+dawndst@users.noreply.github.com>
2022-10-30 03:30:21 +0800

    Update douyu.ts

--- src/engine/website/douyu.ts
+++ src/engine/website/douyu.ts
@@ -51,7 +51,7 @@ export function main(url: string) {
                         version = version.toString().substr(0, version.toString().length - 4)
 
 
-                        const post_data: string = `cdn=tct-h5&did=${device_id}&iar=0&ive=0&rate=0&v=${version}&tt=${request_time}&sign=${sign}`
+                        const post_data: string = `cdn=tct-h5&did=${device_id}&iar=0&ive=0&rate=4&v=${version}&tt=${request_time}&sign=${sign}`
                         axios
                             .post(`https://www.douyu.com/lapi/live/getH5Play/${rid}`, post_data, {
                                 headers: {
